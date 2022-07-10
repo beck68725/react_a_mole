033c52ec7446573c169a6903b78a5f174a9c4978
Rebecca Coburn <rebeccacoburn@Rebeccas-iMac.local>
2022-07-10 14:05:18 -0500

    initial commit

---
+++ src/components/EmptySlot.js
@@ -0,0 +1,23 @@
+import {useEffect} from 'react'
+import molehillImg from '../molehill.png'
+
+function EmptySlot(props){
+    useEffect(() => {
+        // Feel free to adjust this number as you see fit.
+        // It is your game to tune!
+        let randSeconds = Math.ceil(Math.random() * 5000)
+        let timer = setTimeout(() => {
+            props.setDisplayMole(true)
+        }, randSeconds)
+        return () => clearTimeout(timer)
+    })
+
+    return (
+        <div>
+            <img style={{'width': '30vw'}}
+            src={molehillImg} alt=''
+            />
+        </div>
+    )
+}
+export default EmptySlot
